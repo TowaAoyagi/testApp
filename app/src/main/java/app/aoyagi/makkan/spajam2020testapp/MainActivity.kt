08905c4e235933846bb8787838ae8f57738c7553
TowaAoyagi <tab.git.kamui@gmail.com>
2020-10-04 01:42:20 +0900

    input

--- app/src/main/java/app/aoyagi/makkan/spajam2020testapp/MainActivity.kt
+++ app/src/main/java/app/aoyagi/makkan/spajam2020testapp/MainActivity.kt
@@ -1,11 +1,113 @@
 package app.aoyagi.makkan.spajam2020testapp
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.SparseIntArray
+import android.view.Surface
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.Pose
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.PoseDetector
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var uri: Uri
+
+    lateinit var bitmap: Bitmap
+    val REQUEST_VIDEO_CAPTURE = 1
+    private val ORIENTATIONS = SparseIntArray()
+
+    init {
+        ORIENTATIONS.append(Surface.ROTATION_0, 0)
+        ORIENTATIONS.append(Surface.ROTATION_90, 90)
+        ORIENTATIONS.append(Surface.ROTATION_180, 180)
+        ORIENTATIONS.append(Surface.ROTATION_270, 270)
+    }
+
+    /**
+     * Get the angle by which an image must be rotated given the device's current
+     * orientation.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Throws(CameraAccessException::class)
+    private fun getRotationCompensation(cameraId: String, activity: Activity, isFrontFacing: Boolean): Int {
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        val deviceRotation = activity.windowManager.defaultDisplay.rotation
+        var rotationCompensation = ORIENTATIONS.get(deviceRotation)
+
+        // Get the device's sensor orientation.
+        val cameraManager = activity.getSystemService(CAMERA_SERVICE) as CameraManager
+        val sensorOrientation = cameraManager
+            .getCameraCharacteristics(cameraId)
+            .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
+
+        if (isFrontFacing) {
+            rotationCompensation = (sensorOrientation + rotationCompensation) % 360
+        } else { // back-facing
+            rotationCompensation = (sensorOrientation - rotationCompensation + 360) % 360
+        }
+        return rotationCompensation
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        camera.setOnClickListener {
+            Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+                takeVideoIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+                }
+            }
+
+
+
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        val options = PoseDetectorOptions.Builder()
+            .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+            .build()
+        val poseDetector = PoseDetection.getClient(options)
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            if (intent != null) {
+                uri = intent.data!!
+            }
+            bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            imageFromBitmap(bitmap,poseDetector)
+        }
+    }
+    private fun imageFromBitmap(bitmap: Bitmap,poseDetector:PoseDetector) {
+        val rotationDegrees = 0
+        // [START image_from_bitmap]
+        val image = InputImage.fromBitmap(bitmap, 0)
+        // [END image_from_bitmap]
+        Task<Pose> result: = poseDetector.process(image)
+            .addOnSuccessListener { results ->
+                val allPoseLandmarks = pose.getAllPoseLandmarks()
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                // ...
+            }
+    }
+
 }
