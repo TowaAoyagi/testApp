ec288356623b17d3f957bab1ba9fc0ee50769ff4
TowaAoyagi <tab.git.kamui@gmail.com>
2020-10-03 21:57:30 +0900

    add Camera record

--- app/src/main/java/app/aoyagi/makkan/spajam2020testapp/MainActivity.kt
+++ app/src/main/java/app/aoyagi/makkan/spajam2020testapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package app.aoyagi.makkan.spajam2020testapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat.startActivityForResult
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_VIDEO_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        camera.setOnClickListener {
+            Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+                takeVideoIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+                }
+            }
+        }
     }
+
+
 }
