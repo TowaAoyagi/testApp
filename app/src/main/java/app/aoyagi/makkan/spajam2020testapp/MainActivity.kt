c8f0215431fb368a729c57c1775b0d6183457f7e
TowaAoyagi <tab.git.kamui@gmail.com>
2020-10-03 23:10:24 +0900

    add posenet

--- app/src/main/java/app/aoyagi/makkan/spajam2020testapp/MainActivity.kt
+++ app/src/main/java/app/aoyagi/makkan/spajam2020testapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package app.aoyagi.makkan.spajam2020testapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private fun drawableToBitmap(drawable: Drawable): Bitmap {
+        val bitmap = Bitmap.createBitmap(257, 257, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+
+        drawable.draw(canvas)
+        return bitmap
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        camera.setOnClickListener {
+
+            val posenet = Posenet(this.applicationContext)
+            val person = posenet.estimateSinglePose(imageBitmap)
+        }
+
+
     }
 }
